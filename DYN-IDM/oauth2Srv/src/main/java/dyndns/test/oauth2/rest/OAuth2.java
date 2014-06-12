3681d394818a5615786922230b9059f0aaf18286
Pavel_Arlovich <Pavel_Arlovich@epam.com>
2014-06-12 14:01:18 +0300

    initial files for oauth service

--- DYN-IDM/oauth2Srv/src/main/java/dyndns/test/oauth2/rest/OAuth2.java
+++ DYN-IDM/oauth2Srv/src/main/java/dyndns/test/oauth2/rest/OAuth2.java
@@ -1,5 +1,25 @@
 package dyndns.test.oauth2.rest;
 
-public interface OAuth2 {
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
+@Path("oauth")
+public interface OAuth2 {
+	@Path("authorize")
+	@GET
+	@Produces({ MediaType.TEXT_HTML})
+	void authorize(String clientId, String redirectURI);
+
+	@Path("token")
+	@POST
+	@Produces({ MediaType.APPLICATION_JSON })
+	Token getToken(String grantType, String scope, String code);
+
+	@Path("validate")
+	@GET
+	@Produces({ MediaType.APPLICATION_JSON})
+	void validate();
 }
